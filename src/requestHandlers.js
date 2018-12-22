eab3f4a46a7d0a0c04624aa0653a3ff63912caf8
andresperezceron <aperezlevi@gmail.com>
2018-12-22 19:22:42 +0100

    wwww.index

--- src/requestHandlers.js
+++ src/requestHandlers.js
@@ -15,9 +15,53 @@ function staticFileByExtension(response, pathname, contentType) {
 }
 
 function iniciar(response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hola Iniciar");
-    response.end();
+   /* var fs = require("fs");
+    fs.readFile("../www/index.html", function(bError, content) {
+        if(bError) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200,{"Content-Type" : "text/html"});
+            response.end(content);
+        }
+    });*/
+
+    var fs = require("fs");
+    var rootDir = "../www/";
+    var dirs = [];
+
+    function getDirs(callback) {
+        fs.readdir(rootDir, function(err, files) {
+            for(var i = 0; i < files.length; ++i) {
+                var file = files[i];
+                var filePath = rootDir + file;
+                fs.stat(filePath, function(err, stat) {
+                    if(stat.isDirectory()) {
+                        dirs.push(this.file);
+                    }
+                    if(files.length === (this.i + 1)) {
+                        callback(dirs);
+                    }
+                }.bind({file : file, i: i}));
+            }
+        });
+    }
+    getDirs(function (directories) {
+        fs.readFile("../www/html5css3/index.html", function(bError, content) {
+            if(bError) {
+                response.writeHead(500);
+                response.end();
+            }
+            else {
+                response.writeHead(200,{"Content-Type" : "text/html"});
+                response.end(content);
+            }
+        });
+        //response.writeHead(200, {"Content-Type": "text/html"});
+        //response.end(directories.toString());
+    });
+
 }
 
 function subir(response) {
